52d4f665faa3e97184fe6d66944f8f4fcd3da4c1
tfa27 <57012967+tfa27@users.noreply.github.com>
2020-03-20 17:38:31 +0000

    Update question_one.py

--- question_one.py
+++ question_one.py
@@ -1,4 +1,7 @@
-def stringpermcheck(str1, str2):  # create function for the sake of tidiness and ability to call upon
+#  this was a coding challenge for a job I went for.
+
+
+def string_perm_check(str1, str2):  # create function for the sake of tidiness and ability to call upon
     permed = True  # set variable to True initially
     for i in str1:  # iterate through str1, could be str2 doesn't matter which way round it is done
         if str1.count(i) != str2.count(i):  # count each character in each string and check the count is the same.
@@ -7,12 +10,13 @@ def stringpermcheck(str1, str2):  # create function for the sake of tidiness and
         permed = False  # if lengths are different they cannot be permutations
     return permed  # return output variable
 
+
 # computation complexity at a minimum, only two iterative steps to determine if the
 # strings are permutations of one another
 inp1 = 'this is a test to check if these strings are permutations'
-inp2 = 'permutations are strings these if check to test a is this! 3 '
+inp2 = 'permutations are strings these if check to test a is this'
 # deliberately added a few more characters and changed order ^
-if stringpermcheck(inp1, inp2) is True:
+if string_perm_check(inp1, inp2) is True:
     print('These strings are permutations of one another!')
 else:
     print('These strings are not permutations of one another!')
