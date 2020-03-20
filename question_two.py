babc96407184f9dca877ece9b58a4982e764cc0d
tfa27 <57012967+tfa27@users.noreply.github.com>
2020-03-20 17:41:22 +0000

    Update question_two.py

--- question_two.py
+++ question_two.py
@@ -1,4 +1,8 @@
-def questiontwo(list1):
+#  Another question I did for a job a while back. 
+#  Had to take a list of numbers between 0 and 1 million, and count all the missing ones. 
+
+
+def question_two(list1):
     ints = list(range(1, 1000001))  # list in the appropriate range
     for i in list1:  # iterate through input list
         if i in ints:  # if statement to handle any duplicates in input list
@@ -6,11 +10,11 @@ def questiontwo(list1):
     return ints  # return the ints variable containing all missing numbers in the appropriate range
 
 
-def validate(inputlist, outputlist):  # function to validate the above function
+def validate(input_list, output_list):  # function to validate the above function
     check = []  # create a list
     val = bool  # initiate output variable
-    for i in inputlist:  # iterate through the shorter list
-        if i in outputlist:  # iterate through longer list to check no same values exist in both
+    for i in input_list:  # iterate through the shorter list
+        if i in output_list:  # iterate through longer list to check no same values exist in both
             check.append(i)  # if same value in both lists, append value to check list
     if len(check) == 0:  # if the check list is empty, the function has worked
         val = True  # set val to true meaning the function has worked
@@ -19,7 +23,7 @@ def validate(inputlist, outputlist):  # function to validate the above function
     return val
 
 
-def createlist():
+def create_list():
     # creating a random list of integers in appropriate range
     from random import randint
     lst = []
@@ -30,9 +34,9 @@ def createlist():
     return lst
 
 
-inplist = createlist()
+inp_list = create_list()
 # using a second function to double check the lists don't have any of the same values
-if validate(inplist, questiontwo(inplist)) is True:
+if validate(inp_list, question_two(inp_list)) is True:
     print('Program works.')
 else:
     print('Program failed.')
